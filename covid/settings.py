06e51074ace8a54fdf4efeb3b83dc00f584a2068
dearcharlyn <villavicenciocharlyn@gmail.com>
2021-08-25 00:10:30 +0800

    Collect Static

--- covid/settings.py
+++ covid/settings.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
